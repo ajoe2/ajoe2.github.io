72721fcf0f3f70570d395ab4888719945c79b04c
ajoe2 <ajoe2468@gmail.com>
2025-01-15 15:58:38 -0800

    Changed entire website

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,3 @@
 /* 
 Basic JavaScript code for website
 */
-
-let button = document.getElementById("button")
-let output = document.getElementById("buttonoutput");
-let message = "Text generated using JavaScript";
-
-function toggle_text() {
-    curr_button_msg = button.innerHTML
-    if (curr_button_msg === "Generate text") {
-        button.innerHTML = "Erase text"
-        output.innerHTML = message
-    } else {
-        button.innerHTML = "Generate text"
-        output.innerHTML = ""
-    }
-}
-
-button.addEventListener('click', toggle_text)
